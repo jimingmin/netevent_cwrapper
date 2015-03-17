aad1b5665f67d484a0ed5daf9ad122c58d8df8a0
jimingmin <jimingmin@gmail.com>
2015-03-17 13:52:23 +0800

    网络定时器实现
    使用链表按结束时间从小到大排列

---
+++ net_timer.c
@@ -0,0 +1,145 @@
+/*
+ * net_timer.cpp
+ *
+ *  Created on: Mar 17, 2015
+ *      Author: jimm
+ */
+
+#include "net_timer.h"
+#include "list.h"
+#include "net_ctype.h"
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+EXPORT struct list_head *g_pNetTimerList;
+
+int32_t net_timer_init()
+{
+	g_pNetTimerList = (struct list_head *)malloc(sizeof(struct list_head));
+	INIT_LIST_HEAD(g_pNetTimerList);
+	return 0;
+}
+
+int32_t net_timer_uninit()
+{
+	struct list_head *pos, *backup;
+	NetTimer *net_timer;
+
+	list_for_each_safe(pos, backup, g_pNetTimerList)
+	{
+		net_timer = list_entry(pos, NetTimer, list);
+		list_del(&net_timer->list);
+		free(net_timer);
+	}
+
+
+	return 0;
+}
+
+int32_t net_create_timer(TimerProc Proc, void *pTimerData, int32_t nCycleTime, int32_t bIsLoop)
+{
+	time_t now;
+	time(&now);
+
+	NetTimer *net_timer = (NetTimer *)malloc(sizeof(NetTimer));
+	net_timer->Proc = Proc;
+	net_timer->pTimerData = pTimerData;
+	net_timer->nCycleTime = nCycleTime;
+	net_timer->bIsLoop = bIsLoop;
+	net_timer->nEndTime = now + nCycleTime;
+
+	net_insert_timer(net_timer);
+
+	return 0;
+}
+
+int32_t net_destroy_timer(NetTimer *net_timer)
+{
+	list_del(&net_timer->list);
+
+	free(net_timer->pTimerData);
+	free(net_timer);
+
+	return 0;
+}
+
+int32_t net_destroy_timer_by_sessionid(SessionID nSessionID)
+{
+	NetTimer *find_net_timer = net_find_timer(nSessionID);
+	if(find_net_timer != NULL)
+	{
+		net_destroy_timer(find_net_timer);
+	}
+	return 0;
+}
+
+int32_t net_insert_timer(NetTimer *net_timer)
+{
+	struct list_head *pos, *backup;
+	NetTimer *find_net_timer;
+
+	list_for_each_safe(pos, backup, g_pNetTimerList)
+	{
+		find_net_timer = list_entry(pos, NetTimer, list);
+		if(find_net_timer->nEndTime >= net_timer->nEndTime)
+		{
+			list_add_tail(&net_timer->list, &find_net_timer->list);
+			break;
+		}
+	}
+
+	return 0;
+}
+
+NetTimer *net_find_timer(SessionID nSessionID)
+{
+	struct list_head *pos, *backup;
+	NetTimer *find_net_timer;
+
+	find_net_timer = NULL;
+	list_for_each_safe(pos, backup, g_pNetTimerList)
+	{
+		find_net_timer = list_entry(pos, NetTimer, list);
+		struct HeartbeatTimerData *pTimerData = (struct HeartbeatTimerData *)(find_net_timer->pTimerData);
+		if(pTimerData->nSessionID == nSessionID)
+		{
+			break;
+		}
+	}
+
+	return find_net_timer;
+}
+
+int32_t net_loop_timer()
+{
+	if(list_empty(g_pNetTimerList))
+	{
+		return 0;
+	}
+
+	struct list_head *pos, *backup;
+	NetTimer *net_timer;
+
+	time_t now;
+	time(&now);
+
+	list_for_each_safe(pos, backup, g_pNetTimerList)
+	{
+		net_timer = list_entry(pos, NetTimer, list);
+		(*net_timer->Proc)(net_timer->pTimerData);
+
+		if(net_timer->bIsLoop != 0)
+		{
+			net_timer->nEndTime = net_timer->nCycleTime + now;
+			net_insert_timer(net_timer);
+		}
+		else
+		{
+			net_destroy_timer(net_timer);
+		}
+	}
+
+	return 0;
+}
+
---
+++ net_timer.h
@@ -0,0 +1,44 @@
+/*
+ * net_timer.h
+ *
+ *  Created on: Mar 17, 2015
+ *      Author: jimm
+ */
+
+#ifndef NET_TIMER_H_
+#define NET_TIMER_H_
+
+#include "../common/common_export.h"
+#include "../netevent/net_typedef.h"
+#include "list.h"
+
+typedef int32_t (*TimerProc)(void *pTimerData);
+
+typedef struct tagNetTimer
+{
+	TimerProc			Proc;
+	void				*pTimerData;
+	int32_t				nCycleTime;
+	int32_t				bIsLoop;
+	int32_t				nEndTime;
+	struct list_head	list;
+}NetTimer;
+
+int32_t net_timer_init();
+
+int32_t net_timer_uninit();
+
+int32_t net_create_timer(TimerProc Proc, void *pTimerData, int32_t nCycleTime, int32_t bIsLoop);
+
+int32_t net_destroy_timer(NetTimer *net_timer);
+
+int32_t net_destroy_timer_by_sessionid(SessionID nSessionID);
+
+int32_t net_insert_timer(NetTimer *net_timer);
+
+NetTimer *net_find_timer(SessionID nSessionID);
+
+int32_t net_loop_timer();
+
+
+#endif /* NET_TIMER_H_ */
